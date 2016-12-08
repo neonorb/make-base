385b5d48e9e5af472c06dba26056a8adc1e05d08
Chris Smith <chris@hichris.com>
2016-12-08 14:04:34 -0500

    Added pull request merging script to makefile.

--- make-base.mk
+++ make-base.mk
@@ -32,3 +32,16 @@ build/%.o: src/%.s | $$(dir $$@)/.dirstamp
 .PHONY:
 clean:
 	@rm -rf build
+
+# ---- Git ----
+
+.PHONY:
+merge-pullrequest:
+	ifndef MERGE_USER
+	$(error MERGE_USER is not set)
+	endif
+	BRANCH=`git rev-parse --abbrev-ref HEAD`
+	git checkout -b $(MERGE_USER)-$(BRANCH) $(BRANCH) && \
+	git pull git@github.com:$(MERGE_USER)/$(NAME) $(BRANCH) && \
+	git checkout $(BRANCH) && \
+	git merge --no-ff $(MERGE_USER)-$(BRANCH)
---
+++ makefile
@@ -0,0 +1,5 @@
+# Makefile for Make Base
+
+NAME=make-base
+
+-include ../make-base/make-base.mk
