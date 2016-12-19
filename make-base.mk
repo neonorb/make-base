dc8a619d580071b75cb7aae6fb6d6dd5c00640b8
Chris Smith <chris@hichris.com>
2016-12-19 17:08:45 -0500

    Moved -fno-rtti and -fno-exceptions.

--- make-base.mk
+++ make-base.mk
@@ -12,7 +12,7 @@ TFLAGS=$(if $(ALLOW_TEST),-D ALLOW_TEST)
 INCLUDE_FLAGS=-I include $(patsubst %, -I ../%/include, $(LIBS))
 LIBS_FLAGS=$(foreach L, $(LIBS), -L ../$L/build -l$L)
 
-CFLAGS=-c -std=c++14 -fpic -Wall -Wextra -Wno-comment -fno-stack-protector -fshort-wchar -mno-red-zone -DEFI_FUNCTION_WRAPPER -fno-exceptions -fno-rtti $(INCLUDE_FLAGS) $(LIBS_FLAGS) $(CDFLAGS) $(CTFLAGS) $(TFLAGS)
+CFLAGS=-c -std=c++14 -fpic -Wall -Wextra -Wno-comment -fno-stack-protector -fshort-wchar -mno-red-zone -DEFI_FUNCTION_WRAPPER $(INCLUDE_FLAGS) $(LIBS_FLAGS) $(CDFLAGS) $(CTFLAGS) $(TFLAGS)
 AFLAGS=-f elf64
 
 all: $(OBJECTS)
