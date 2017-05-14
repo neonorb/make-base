314882c4533e3f7b818687ef3f5408692af3b1c2
Chris Smith <chris@hichris.com>
2017-05-14 17:13:43 -0400

    Changed tmp file name to be more agreeable with parellel builds.

--- make-lib.mk
+++ make-lib.mk
@@ -2,9 +2,10 @@ ARFLAGS=
 
 all: lib
 lib: $$(foreach A, $(ARCHS), build/$$A/lib$(NAME).a)
+TMP_AR_OUT=/tmp/asiago_$(NAME)_ar_output.tmp
 build/%/lib$(NAME).a: $$(OBJECTS-$$(CURRENT_ARCH)) | $$(dir $$@)/.dirstamp
 	$(NO_PRINT_COMMAND)$(RR-$(CURRENT_ARCH)) \
 	    -r $@ \
 	    $^ \
 	    $(ARFLAGS) \
-	    2> /tmp/makefilearoutput || (cat /tmp/makefilearoutput ; rm /tmp/makefilearoutput ; exit 1) && rm /tmp/makefilearoutput
+	    2> $(TMP_AR_OUT) || (cat $(TMP_AR_OUT) ; rm $(TMP_AR_OUT) ; exit 1) && rm $(TMP_AR_OUT)
